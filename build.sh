1989c39b8c86857baae4f5e899c83914feb64aa5
idk2137 <igorekxsuper@gmail.com>
2026-08-04 05:05:59 +0200

    Add print_int with digit reversal

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 nasm -f elf32 boot.asm -o boot.o
-gcc -m32 -ffreestanding -fno-pic -c kernel.c -o kernel.o
+gcc -m32 -ffreestanding -fno-pic -fno-stack-protector -c kernel.c -o kernel.o
 ld -m elf_i386 -T linker.ld -o kernel.bin boot.o kernel.o --oformat binary
 qemu-system-x86_64 kernel.bin
--- kernel.c
+++ kernel.c
@@ -1,17 +1,48 @@
 
-  void print(char *message, char color){
-    char *video_memory = (char *) 0xB8000;
-    int i = 0;
-    while (message[i] != '\0') {
-    video_memory[i * 2] = message[i];
-    video_memory[i * 2 + 1] = color;
+int cursor_position = 0;
+
+
+void print(char *message, char color) {
+  char *video_memory = (char *) 0xB8000;
+  int i = 0;
+  while (message[i] != '\0') {
+    video_memory[cursor_position * 2] = message[i];
+    video_memory[cursor_position * 2 + 1] = color;
+    cursor_position++;
     i++;
     }
+}
+
+void print_digit(int digit, char color) {
+  char c = '0' + digit;
+  char str[2];
+  str[0] = c;
+  str[1] = '\0';
+  print(str, color);
+}
+
+void print_int (int num, char color) {
+  char digits[12];
+  int tmp;
+  int i = 0;
+  while (num != 0) {
+    tmp = num % 10;
+    digits[i] = '0' + tmp;
+    num = num / 10;
+    i++;  
   }
-
-
+  while (i > 0) {
+  i--;  
+  char c = digits[i];
+  char one_char[2] = {c, '\0'};
+  print(one_char, color);
+  }
+}
 
 void kernel_main() {
   print("Welcome to my Kernel OS!", 0x0B);
+  print_digit(7, 0x0B);
+  int num = 123;
+  print_int(num, 0x0B);
   while (1) {}
 }
